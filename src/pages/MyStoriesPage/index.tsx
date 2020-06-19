80e2e9dca60a60de0257dac7d609321fc7fc4533
faishasj <faishasj@gmail.com>
2020-06-19 13:15:36 +1000

    feat(MyStories): Add edit and delete functionality

--- src/pages/MyStoriesPage/index.tsx
+++ src/pages/MyStoriesPage/index.tsx
@@ -12,19 +12,30 @@ import { Story } from '../../constants/Types';
 import Header from '../../components/Header';
 import StoryCard from '../../components/StoryCard';
 import CircleButton from '../../components/CircleButton';
+import Modal from '../../components/Modal';
+import Link from '../../components/Link';
 import './MyStoriesPage.css';
 
 
 const MyStoriesPage: React.FC<RouteComponentProps> = ({ history }) => {
   const messengerSDK: any = useContext(MessengerContext);
 
-  const [authorID, setAuthorID] = useState<string>('');
+  const [authorID, setAuthorID] = useState<string>('3933693980036784');
   const [stories, setStories] = useState<Story[]>([]);
+  const [deletingStory, setDeletingStory] = useState<Story | null>(null);
 
   const setMyStories = useCallback(async () => {
     const myStories = await getMyStories(authorID);
     setStories(myStories);
   }, [authorID])
+
+  const editStory = useCallback(storyID => {
+    history.push(ROUTES.DM_CREATOR, { storyID });
+  }, [])
+
+  const deleteStory = useCallback(storyID => {
+    
+  }, [])
 
   useEffect(() => {
     if (messengerSDK) {
@@ -47,23 +58,32 @@ const MyStoriesPage: React.FC<RouteComponentProps> = ({ history }) => {
         <Header pageTitle="My Stories"/>
         <div className="container">
           <div className="storyList">
-            {stories.map(({published, metadata: { title, description, genre, coverPhoto }}, key) => (
+            {stories.map((story, key) => (
               <StoryCard 
                 key={ key }
                 isLibrary={ false }
-                title={ title } 
-                description={ description } 
-                genre={ genre } 
-                coverPhoto={ coverPhoto }
-                published={ published }
-                editCallback={ () => alert("HI") }
-                deleteCallback={ () => alert("TEST") }
+                title={ story.metadata.title } 
+                description={ story.metadata.description } 
+                genre={ story.metadata.genre } 
+                coverPhoto={ story.metadata.coverPhoto }
+                published={ story.published }
+                editCallback={ () => editStory(story.id) }
+                deleteCallback={ () => setDeletingStory(story) }
               />
             ))}
           </div>
+
           <div className="newStoryButton">
             <CircleButton icon="✍️" onClick={() => history.push(ROUTES.DM_CREATOR)}/>
           </div>
+          
+          {deletingStory && (
+            <Modal prompt={`Are you sure you want to delete ${deletingStory?.metadata.title}?`} 
+            buttons={<>
+              <Link label="CANCEL" onClick={() => setDeletingStory(null)}/>
+              <Link label="DELETE" onClick={() => deleteStory(deletingStory.id)}/>
+            </>}/>
+          )}
         </div>
     </div>
   );
