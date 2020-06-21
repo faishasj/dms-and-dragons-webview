a9ee451e063203ae62b72d4bfd95f1d42db9aad0
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-21 22:10:22 +1000

    Fix(FileUpload): Fix erasing most file data with spread sytax

--- src/hooks/useFileUpload.ts
+++ src/hooks/useFileUpload.ts
@@ -29,7 +29,7 @@ const useFileUpload = (
         return;
       }
       setErrors([]);
-      setFile({ ...acceptedFile, preview: URL.createObjectURL(acceptedFile) });
+      setFile(Object.assign(acceptedFile, { preview: URL.createObjectURL(acceptedFile) }));
     },
     ...dropzoneOptions,
   });
