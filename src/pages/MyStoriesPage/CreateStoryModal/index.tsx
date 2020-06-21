9c42801619958251575b2512ec1e3e17a9e908b1
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-21 21:00:37 +1000

    Feat(CreateStory): Implemen  basic title, genre and description fields

--- src/pages/MyStoriesPage/CreateStoryModal/index.tsx
+++ src/pages/MyStoriesPage/CreateStoryModal/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
@@ -9,50 +9,77 @@ import Link from '../../../components/Link';
 import { Story } from '../../../constants/Types';
 
 
+const GENRES = [
+  '🤣 Comedy',
+  '🎭 Drama',
+  '🤔 Educational',
+  '🏰 Fantasy',
+  '😱 Horror',
+  '🧐 Non-fiction',
+  '💕 Romance',
+  '🧪 Sci-fi',
+  '🤠 Western',
+];
+
+
 const CreateStoryModal: React.FC<CreateStoryModalProps> = ({
   onDismiss,
-}) => (
-  <Modal>
-    <div className="newStoryForm">
-      <div className="title">Create a new story!</div>
-      <div className="metadata">
-        <div className="coverPhotoData">
-          <div className="coverPhotoContainer">
-            <FontAwesomeIcon icon={ faCamera }/>
+}) => {
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState(GENRES[0]);
+  const [description, setDescription] = useState('');
+
+  const submit = useCallback(() => {
+    console.log(title, genre, description);
+  }, [title, genre, description]);
+
+  return (
+    <Modal>
+      <div className="newStoryForm">
+        <div className="title">Create a new story!</div>
+        <div className="metadata">
+          <div className="coverPhotoData">
+            <div className="coverPhotoContainer">
+              <FontAwesomeIcon icon={ faCamera }/>
+            </div>
+          </div>
+          <div className="textData">
+            <div><label htmlFor="title">TITLE</label></div>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              maxLength={20} 
+              placeholder="Enter title..."
+              onChange={e => setTitle(e.target.value)}
+            ></input>
+
+            <div><label htmlFor="genre">GENRE</label></div>
+            <select id="genre" name="genre" onChange={e => setGenre(e.target.value)}>
+              {GENRES.map(genreOption => <option key={genreOption} value={genreOption}>{genreOption}</option>)}
+            </select>
           </div>
         </div>
-        <div className="textData">
-          <div><label htmlFor="title">TITLE</label></div>
-          <input id="title" name="title" type="text" maxLength={20} 
-            placeholder="Enter title..."></input>
 
-          <div><label htmlFor="genre">GENRE</label></div>
-          <select id="genre" name="genre">
-            <option value="🤣 Comedy">🤣 Comedy</option>
-            <option value="🎭 Drama">🎭 Drama</option>
-            <option value="🤔 Educational">🤔 Educational</option>
-            <option value="🏰 Fantasy">🏰 Fantasy</option>
-            <option value="😱 Horror">😱 Horror</option>
-            <option value="🧐 Non-fiction">🧐 Non-fiction</option>
-            <option value="💕 Romance">💕 Romance</option>
-            <option value="🧪 Sci-fi">🧪 Sci-fi</option>
-            <option value="🤠 Western">🤠 Western</option>
-          </select>
+        <div className="descriptionContainer">
+          <div><label htmlFor="description">DESCRIPTION</label></div>
+          <textarea
+            rows={4}
+            maxLength={120}
+            name="description"
+            placeholder="Enter description..."
+            onChange={e => setDescription(e.target.value)}
+          />
+        </div>
+
+        <div className="buttons">
+          <Link label="CANCEL" className="cancel" onClick={onDismiss}/>
+          <Link label="NEXT" onClick={submit} />
         </div>
       </div>
-
-      <div className="descriptionContainer">
-        <div><label htmlFor="description">DESCRIPTION</label></div>
-        <textarea rows={4} maxLength={120} name="description" placeholder="Enter description..."/>
-      </div>
-
-      <div className="buttons">
-        <Link label="CANCEL" className="cancel" onClick={onDismiss}/>
-        <Link label="NEXT" onClick={() => console.log('NEXT')} />
-      </div>
-    </div>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export interface CreateStoryModalProps {
   onDismiss: () => void;
