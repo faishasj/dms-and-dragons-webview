c1b3edd89eb3abf7eda63cf534106373253b333f
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-21 21:56:29 +1000

    Refactor(Database): Add typing to data serializer

--- src/lib/Database.ts
+++ src/lib/Database.ts
@@ -3,25 +3,25 @@ import { db } from './Firebase';
 import { Story } from '../constants/Types';
 
 
-function serialize(doc: firestore.DocumentSnapshot): any {
+function serialize<T extends { id: string }>(doc: firestore.DocumentSnapshot): T {
   return {
     id: doc.id,
     ...doc.data(),
-  };
+  } as T;
 }
 
 export async function getMyStories(authorId: Story['authorId']): Promise<Story[]> {
   const storiesQuery = await db.collection('stories')
                                .where('authorId', '==', authorId)
                                .orderBy('dateUpdated', 'desc').get();
-  return storiesQuery.docs.map(doc => serialize(doc));
+  return storiesQuery.docs.map(doc => serialize<Story>(doc));
 }
 
 export async function getLibrary(): Promise<Story[]> {
   const storiesQuery = await db.collection('stories')
                                .where('published', '==', true)
                                .orderBy('dateUpdated', 'desc').get()
-  return Promise.all(storiesQuery.docs.map(doc => serialize(doc) as Story)
+  return Promise.all(storiesQuery.docs.map(doc => serialize<Story>(doc))
         .map(async (story) => {
           const author = await db.collection('users').doc(story.authorId).get();
           return { ...story, authorName: author.data()?.name || ''};
