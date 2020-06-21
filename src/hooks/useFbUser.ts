834b59c15766e15d59b3bc73bab5fb7c6c2e81ad
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-21 18:00:07 +1000

    Project(dev): Set useFbUser to use hardcoded value in development

--- src/hooks/useFbUser.ts
+++ src/hooks/useFbUser.ts
@@ -6,6 +6,7 @@ export const useFbUser = (callback: (fbId: string) => void): void => {
   const messengerSDK: any = React.useContext(MessengerContext);
 
   React.useEffect(() => {
+    if (process.env.NODE_ENV === 'development') callback('3933693980036784');
     if (messengerSDK) {
       messengerSDK?.getContext('256197072270291',
         async ({ psid }: ThreadContext) => {
