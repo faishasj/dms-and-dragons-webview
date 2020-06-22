bc13ef28c9fb1f462ec66929920785fade7c083f
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-23 01:32:42 +1000

    Feat(DMCreator): Add simple editor expanding box

--- src/pages/DMCreatorPage/Message/index.tsx
+++ src/pages/DMCreatorPage/Message/index.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Message.css';
 
 
 const Message: React.FC<MessengerProps> = ({ isAuthor, message, name, profilePicture }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className={"Message" + (isAuthor ? " author": "")}>
-      { profilePicture && !isAuthor && (
-        <img className="authorPic" alt="Author" src={ profilePicture }/>
-      )}
+    <div className={"Message" + (isAuthor ? " author": "")} onClick={() => setExpanded(!expanded)}>
+      {profilePicture && !isAuthor && <img className="authorPic" alt="Author" src={profilePicture}/>}
       <div className="messageBody">
-        { name && !isAuthor && (
-          <div className="authorName">{ name }</div>
-        )}
-        <div className="messageText">{ message }</div>
+        {name && !isAuthor && <div className="authorName">{name}</div>}
+        <div className="messageText">{message}</div>
       </div>
+      {expanded && (
+        <div className="expandedContainer">
+          <p>Expanded</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export interface MessengerProps {
   isAuthor: boolean,
