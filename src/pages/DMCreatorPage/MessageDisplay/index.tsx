cc30f798df90feadd7ae0265bf825f5875436c74
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-24 21:31:47 +1000

    Fix(Message Editing): Fix issues with message editing

--- src/pages/DMCreatorPage/MessageDisplay/index.tsx
+++ src/pages/DMCreatorPage/MessageDisplay/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 
 import ContentEditable from 'react-contenteditable';
 
@@ -12,25 +12,25 @@ const MessageDisplay: React.FC<MessageDisplayProps> = ({
 }) => {
   const [expanded, setExpanded] = useState(false);
 
-  const [text, setText] = useState(messageText || message?.text);
-  const [image, setImage] = useState(message?.image);
-  const [waitTime, setWait] = useState(message ? message?.waitingTime / 1000 : 0);
-  const [typeTime, setType] = useState(message ? message?.typingTime / 1000 : 0);
+  const update = useCallback((key: keyof Message, value: any) => onUpdate?.({ ...message, [key]: value }), [message, onUpdate]);
+  const updateTime = useCallback((key: keyof Message, value: number) => {
+    if (value > 100 || value < 0 || (value.toString().length > 3)) return;
+    return update(key, (value * 1000) || 0);
+  }, [update]);
+
+  const waitTime = (message?.waitingTime || 0) / 1000;
+  const typeTime = useMemo(() => (message?.typingTime || 0) / 1000, [message]);
 
   return (
     <div className={"Message"}>
       {profilePicture && <img className="authorPic" alt="Author" src={profilePicture}/>}
-      <div className="messageBody" onClick={() => {
-          setExpanded(!expanded);
-          if (expanded) onUpdate?.({ ...message, text, image, typingTime: typeTime, waitingTime: waitTime });
-        }}
-      >
+      <div className="messageBody" onClick={() => setExpanded(!expanded)}>
         {name && <div className="authorName">{name}</div>}
         
         <ContentEditable
           className="messageText" 
-          html={text!}
-          onChange={e => setText(e.target.value)}
+          html={messageText || message?.text || ''}
+          onChange={e => update('text', e.target.value)}
         />
       </div>
 
@@ -38,12 +38,24 @@ const MessageDisplay: React.FC<MessageDisplayProps> = ({
         <div className="expandedContainer">
           <div className="row">
             <div>Wait </div>
-              <input type="number" min={0.1} value={waitTime?.toString()} onChange={e => setWait(parseFloat(e.target.value) || 0)} />
+              <input
+                type="number"
+                min={0.1}
+                max={100}
+                value={waitTime.toString()}
+                onChange={e => updateTime('waitingTime', parseFloat(e.target.value))}
+              />
             <div> second{typeTime === 1 ? '' : 's'}</div>
           </div>
           <div className="row">
             <div>Type for </div>
-            <input type="number" value={typeTime?.toString()} onChange={e => setType(parseFloat(e.target.value) || 0)} />
+            <input
+              type="number"
+              min={0.1}
+              max={100}
+              value={typeTime.toString()}
+              onChange={e => updateTime('typingTime', parseFloat(e.target.value))}
+            />
             <div> second{typeTime === 1 ? '' : 's'}</div>
           </div>
         </div>
